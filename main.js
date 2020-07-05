65f1e096a0054b8edfffd857e536a71e5d06a380
lanshaolan <xanxuslan@gmail.com>
2020-07-05 12:58:47 +0800

    過程

---
+++ main.js
@@ -0,0 +1,35 @@
+console.log("我是 JS!!!");
+
+// DOM 文件物件模式
+// 文件.透過 ID 搜尋元素("ID 名稱").插入 HTML = "文字訊息";
+document.getElementById("box1").innerHTML = "哈囉~";
+
+// 變數 variable
+// 用於儲存資料
+// 資料類型
+var a = 10;         // 整數
+var b = 1.5;        // 浮點數
+var c = "文字";     // 字串
+var d = true;       // 布林值 true 是、false 否
+
+console.log(a);
+
+// 運算子
+// 數學 加減乘除餘 + - * / %
+
+// 程式內的 = 指定符號
+// = 右邊先執行再指定給左邊
+a = a % 3;
+
+console.log(a);
+
+// HTML5，CSS，JS ES6
+// ++ 遞增1 -- 遞增1
+var e = 7;
+e++;
+console.log(e);
+
+// ES6 指數符號 **
+var f = 5;
+f = f**3;
+console.log(f);
